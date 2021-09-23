461e76c43146fd3111ce439935334b677db36551
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-23 10:49:48 +0200

    Create 'Create" component

---
+++ src/pages/Create.js
@@ -0,0 +1,16 @@
+import Typography from '@material-ui/core/Typography';
+
+export default function Create() {
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+    </div>
+  );
+}
