968ae781364db2912f95e471f44922222f8250be
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-28 13:58:41 +0200

    Apply icon to Button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,7 @@
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -19,6 +20,7 @@ export default function Create() {
         type="submit"
         color="secondary"
         variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
