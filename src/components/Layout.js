20ab62b18942825f0f1a1a47583165498a8255c3
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-04 19:05:12 +0200

    Add Drawer component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,22 @@
 import { makeStyles } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
   page: {
     background: '#f9f9f9',
     width: '100%'
+  },
+  drawer: {
+    width: drawerWidth
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  root: {
+    display: 'flex'
   }
 });
 
@@ -11,10 +24,20 @@ export default function Layout({ children }) {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* app bar */}
 
       {/* side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">My Notes</Typography>
+        </div>
+      </Drawer>
 
       <div className={classes.page}>{children}</div>
     </div>
