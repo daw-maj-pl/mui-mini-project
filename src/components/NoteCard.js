d2599f4cac769f531957c7da53bb8272c25a0e74
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-05 18:23:58 +0200

    Add border property to class dynamically

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -4,11 +4,24 @@ import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  test: {
+    border: note => {
+      if (note.category === 'work') {
+        return '1px solid red';
+      }
+    }
+  }
+});
 
 export default function NoteCard({ note, handleDelete }) {
+  const classes = useStyles(note);
+
   return (
     <div>
-      <Card elevation={1}>
+      <Card elevation={1} className={classes.test}>
         <CardHeader
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
